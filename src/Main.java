4c3570ace07c91c99fde4fb82ed891fab16d1e44
EthanNayt <ethanaung260@gmail.com>
2022-09-26 13:02:18 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int arrays[]={1,2,3,4,5};
+        Arrays.sort(arrays);
+        for(int i=0;i<=arrays.length-1;i++){
+            System.out.println(arrays[i]);
+        }
+    }
+}
